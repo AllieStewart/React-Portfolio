bc2e4d3c78b021e1159b68332390a72362da7998
AllieStewart <megaxis1217@gmail.com>
2023-12-03 15:44:00 -0600

    trying to fix up index.css and AboutPage/Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 export default function Header({ links }) {
     return (
-      <nav className="navbar navbar-expand-lg bg-secondary">
+      <header>
+      <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <h1 className="mg-4">Allie Stewart</h1>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               {links.map((link) => link)}
             </ul>
           </div>
         </div>
       </nav>
+      </header>
     );
   }
 // Developer's name, navigation for About Me (Default), Portfolio, Contact, Resume
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,8 +3,12 @@ export default function AboutPage() {
     return (
       <div className="container pt-4">
         <h3>About Me</h3>
-        <p>
-          Some about me text here.
+          <img className="about-img" src="../../public/about/about.jpg" alt=""/>
+        <p className="about-p">
+          My name is Allie Stewart.
+          I was born and raised in Texas.
+          My interests include drawing, video games, videography,
+          and programming.
         </p>
       </div>
     );
