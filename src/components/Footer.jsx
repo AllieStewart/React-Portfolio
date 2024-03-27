4327f1f97c9682999ba9d96eca12d38a4c3694e1
AllieStewart <megaxis1217@gmail.com>
2024-03-26 19:31:03 -0500

    editing Footer.jsx to load logo imgs for GitHub and LinkedIn

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,15 @@
 // Start of JSX file
 // Footer that displays my GitHub and LinkedIn links.
-import githubLogo from '../../../src/assets/portfolio_icons/GH_icon.svg';
-import linkedInLogo from '../../../src/assets/portfolio_icons/LI_icon.png';
 
 export default function Footer() {
     return (
     <footer>
         <div class="footer"><center className="footerIcons">
             <a href="https://github.com/AllieStewart">
-            <img src={githubLogo} className="gitHubIcon" />
+            <img src='../../src/assets/portfolio_icons/GH_icon.svg' className="gitHubIcon" />
             </a>
             <a href="https://www.linkedin.com/in/allie-j-stewart/">
-            <img src={linkedInLogo} height="96px" className="linkedInIcon" />
+            <img src='../../src/assets/portfolio_icons/LI_icon.png' height="96px" className="linkedInIcon" />
             </a>
         </center>
         </div>
