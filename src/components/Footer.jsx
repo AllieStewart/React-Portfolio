de1217243831488155013d3621dfe21c83f1e234
AllieStewart <megaxis1217@gmail.com>
2024-03-26 20:02:58 -0500

    edited Footer.jsx and copied some icons to the /public folder

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ export default function Footer() {
     <footer>
         <div class="footer"><center className="footerIcons">
             <a href="https://github.com/AllieStewart">
-            <img src='../src/assets/portfolio_icons/GH_icon.svg' className="gitHubIcon" />
+            <img src='/public/GH_icon.svg' className="gitHubIcon" />
             </a>
             <a href="https://www.linkedin.com/in/allie-j-stewart/">
-            <img src='../src/assets/portfolio_icons/LI_icon.png' height="96px" className="linkedInIcon" />
+            <img src='/public/LI_icon.png' height="96px" className="linkedInIcon" />
             </a>
         </center>
         </div>
